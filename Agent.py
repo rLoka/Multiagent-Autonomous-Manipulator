1ede758fe02b72e56404971e6db9b3fad1d52f0c
Karlo Grlić <karlogrlicweb@gmail.com>
2018-01-05 20:44:49 +0100

    Python Agent Interface

---
+++ Agent.py
@@ -0,0 +1,32 @@
+from enum import Enum
+
+class AgentType(Enum):
+    KinematicsAgent = 1
+    SensorAgent = 2
+    LearningAgent = 3
+
+class Agent:
+
+    def __init__(self, agentType):
+        self.AgentType = agentType
+        self.PeerAgents = []
+
+    def AddPeerAgent(self, peerAgent):
+        self.PeerAgents.append(peerAgent)
+        self.OnPeerAgentAdded()
+
+    def OnPeerAgentAdded(self):
+        raise NotImplementedError
+
+    def SendMessage(self, receivingAgents, content):
+        for agent in receivingAgents:
+            agent.ReceiveMessage(self, content)
+
+    def ReceiveMessage(self, senderAgent, content):
+        raise NotImplementedError
+
+    def Act(self):
+        raise NotImplementedError
+    
+    def Stop(self):
+        raise NotImplementedError
